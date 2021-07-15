2a7a1f95b84df7e8ec60a8903fdbc62c091062f9
Prem Patel <prempatel_8@outlook.com>
2021-07-15 04:19:11 -0500

    2.5: separate module

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,56 +1,5 @@
 import React from "react";
-
-const Course = ({ courses }) => {
-  return (
-    <div>
-      {courses.map((course) => {
-        return (
-          <div key={course.id}>
-            <Header course={course} />
-            <Content course={course} />
-            <Total course={course} />
-          </div>
-        );
-      })}
-    </div>
-  );
-};
-
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
-};
-
-const Content = ({ course }) => {
-  return (
-    <>
-      {course.parts.map((part) => (
-        <Part part={part} key={part.id} />
-      ))}
-    </>
-  );
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      <span>
-        {part.name} {part.exercises}
-      </span>
-    </p>
-  );
-};
-
-const Total = ({ course }) => {
-  return (
-    <p>
-      <strong>
-        total of{" "}
-        {course.parts.reduce((total, part) => total + part.exercises, 0)}{" "}
-        exercises
-      </strong>
-    </p>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
